f28d2736622ae44c959b12963ced37d9553e6023
dieiverson <you@example.com>
2020-12-15 16:25:07 -0300

    Design contador de pessoas

--- contador_pessoas/lib/main.dart
+++ contador_pessoas/lib/main.dart
@@ -3,11 +3,46 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
       title: "Contador de Pessoas",
-      home: Column(
-        children: <Widget>[
-          Text("Pessoas:0",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold))
-        ],
-      )));
+      home: Home()
+  ));
 }
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset("images/restaurant.jpg", fit: BoxFit.cover,height: 1000.0),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Pessoas:0",
+                style:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Padding(padding: EdgeInsets.all(10.0), child: FlatButton(
+                  onPressed: null,
+                  child: Text("+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white)))),
+              Padding(padding: EdgeInsets.all(10.0), child: FlatButton(
+                  onPressed: null,
+                  child: Text("-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white)))),
+            ]),
+            Text("Pode Entrar",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0))
+          ],
+        )
+      ],
+    );
+  }
+}
+
