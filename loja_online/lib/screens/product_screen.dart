c4161efa683a06ebab70ac759a89bf99c934446d
Dieiverson Kava <38850216+Dieiverson@users.noreply.github.com>
2021-05-05 19:03:32 -0300

    Update product_screen.dart

--- loja_online/lib/screens/product_screen.dart
+++ loja_online/lib/screens/product_screen.dart
@@ -26,16 +26,40 @@ class _ProductScreenState extends State<ProductScreen> {
       ),
       body: ListView(
         children: [
-          AspectRatio(aspectRatio: 0.9,child: Carousel(
-            images: product.images.map((url) {
-              return NetworkImage(url);
-            }).toList(),
-            dotSize: 4.0,
-            dotSpacing: 15.0,
-            dotBgColor: Colors.transparent,
-            dotColor: primaryColor,
-            autoplay: false,
-          ),)
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: product.images.map((url) {
+                return NetworkImage(url);
+              }).toList(),
+              dotSize: 4.0,
+              dotSpacing: 15.0,
+              dotBgColor: Colors.transparent,
+              dotColor: primaryColor,
+              autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  product.title,
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                ),
+                SizedBox(hei)
+              ],
+            ),
+          )
         ],
       ),
     );
