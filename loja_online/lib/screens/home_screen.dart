5ee51bf85dc0062f407cfb33309155cbe15f6b1c
Dieiverson Kava <dieiverson1@gmail.com>
2021-05-06 20:36:38 -0300

    Loja 100% finalizada

--- loja_online/lib/screens/home_screen.dart
+++ loja_online/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_online/main.dart';
 import 'package:loja_online/tabs/home_tab.dart';
+import 'package:loja_online/tabs/orders_tab.dart';
 import 'package:loja_online/tabs/products_tab.dart';
 import 'package:loja_online/widgets/cart_button.dart';
 import 'package:loja_online/widgets/custom_drawer.dart';
@@ -14,20 +15,30 @@ class HomeScreen extends StatelessWidget {
       controller: pageController,
       physics: NeverScrollableScrollPhysics(),
       children: [
-       Scaffold(
-         floatingActionButton: CartButton(),
-        body: HomeTab(),
-         drawer: CustomDrawer(pageController),
-       ),
         Scaffold(
-        floatingActionButton: CartButton(),
+          floatingActionButton: CartButton(),
+          body: HomeTab(),
+          drawer: CustomDrawer(pageController),
+        ),
+        Scaffold(
+          floatingActionButton: CartButton(),
           appBar: AppBar(
             title: Text("Produtos"),
             centerTitle: true,
           ),
           drawer: CustomDrawer(pageController),
           body: ProductsTab(),
+        ),
+        Container(color: Colors.amber),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Meus Pedidos"),
+            centerTitle: true,
+          ),
+          body: OrdersTab(),
+          drawer: CustomDrawer(pageController),
         )
-    ],);
+      ],
+    );
   }
 }
