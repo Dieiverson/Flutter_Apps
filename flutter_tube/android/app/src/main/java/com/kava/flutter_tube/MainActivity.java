6c71c5f840f9014602d999c11faf3b3af74a8369
Dieiverson Kava <38850216+Dieiverson@users.noreply.github.com>
2021-05-07 13:45:50 -0300

    App youtube Iniciado

---
+++ flutter_tube/android/app/src/main/java/com/kava/flutter_tube/MainActivity.java
@@ -0,0 +1,6 @@
+package com.kava.flutter_tube;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutter_tube/lib/api.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:flutter_tube/models/video.dart';
+import 'package:http/http.dart' as http;
+const API_KEY = "AIzaSyCthZbtSZirFWLOkYY9MkOe6bgE6c-M4LA";
+
+class Api{
+  search(String search) async
+  {
+    var url = Uri.parse("https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10");
+    http.Response response = await http.get(url);
+
+    decode(response);
+  }
+ List<Video> decode(http.Response response)
+  {
+    if(response.statusCode == 200){
+        var decoded = json.decode(response.body);
+        List<Video> videos = decoded["items"].map<Video>((map)
+            {
+              return Video.fromJson(map);
+            }
+        ).toList();
+        return videos;
+      }
+    else
+      {
+        throw new Exception("Failed to load videos.");
+      }
+  }
+}
+
+
+//"https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+//"https://www.googleapis.com/youtube/v3/search?part=snippet&q=$_search&type=video&key=$API_KEY&maxResults=10&pageToken=$_nextToken"
+//"http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json"
